0e0148ef7f4eeac5a5aa100cae1d86f398acb5af
omarElemamElmghawry <o.emam@excp.sa>
2025-09-24 16:16:03 +0300

    feat: adding the loading and the not found pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex-col gap-3 flex-center min-h-screen w-screen">
+      <Image
+        src="/images/logo.svg"
+        alt={APP_NAME + " logo"}
+        height={70}
+        width={70}
+      ></Image>
+
+      <div className="shadow-lg p-5 flex-center flex-col gap-4 rounded-md border-0">
+        <p className="text-destructive">This page was not found</p>
+
+        <Button asChild variant={"outline"}>
+          <Link href={"/"}>Back to home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
