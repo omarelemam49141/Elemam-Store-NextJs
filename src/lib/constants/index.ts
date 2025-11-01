6ae7d6249448dec1379baa3f6d77c1a8f152ff4d
omarElemamElmghawry <o.emam@excp.sa>
2025-11-01 14:40:24 +0200

    feat: changing the max cart operation per hour

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -7,6 +7,6 @@ export const NEWEST_ARRIVAL_PRODUCTS_COUNT =
   +env.NEWEST_ARRIVAL_PRODUCTS_COUNT!;
 export const CART_ID_SESSION = "cart_id_session";
 export const CART_RATE_LIMIT_PER_HOUR = parseInt(
-  env.CART_RATE_LIMIT_PER_HOUR || "5",
+  env.CART_RATE_LIMIT_PER_HOUR || "50",
   10
 );
